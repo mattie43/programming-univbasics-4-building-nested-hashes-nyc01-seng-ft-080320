878c6642f80e415dc4acbdb459fa6501f03a9b50
Matt Ericksen <ericksenm93@ymail.com>
2020-07-22 23:59:09 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -32,7 +32,9 @@ def monopoly_with_third_tier
     },
     :names => {
       :reading_railroad => {},
-      :pennsylvania_railroad => {}
+      :pennsylvania_railroad => {},
+      :reading_railroad => {},
+      :reading_railroad => {}
     }
     }
   }
